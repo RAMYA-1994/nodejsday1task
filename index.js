b35796e4ade599e4d91885a0fd124cc5a9495583
RAMYA-1994 <ramyarrr2016@gmail.com>
2024-06-03 13:03:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// console.log("hello world")
+// const date=Date();
+// console.log(date);
+var timestamp = Date.now();
+// console.log(timestamp);
+const data={timestamp};
+const fs=require("fs")
+const path=require("path")
+console.log(data);
+const { isUtf8 } = require('buffer');
+const http=require('http')
+const PORT=7000
+let server= http.createServer((req,res)=>{
+console.log("hello")
+    try{
+
+        let filepath=path.join(__dirname+"/timestamp.txt")
+        let data=fs.writeFileSync(filepath,"hello")
+        // res.WriteHead(200,{'Content-Type':'application/json'})
+        res.end("successfully created") 
+    }catch(error){
+        console.log(error)
+    }
+
+})
+server.listen(PORT,()=>console.log("server listening to"+PORT))
